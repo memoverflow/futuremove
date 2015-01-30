88fd03c139108e9811a3d13ff6b3f9d7d9437bb9
xuren87 <nichokiki@hotmail.com>
2015-01-31 01:02:06 +0800

    修改了一些显示bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /**
- * 创建lnh页面的生命周期
+ * Future Move ohoo!!! :)
  * @authors Lucas Ren (nichokiki@hotmail.com)
  * @date    2015-01-20
  * @version v1.0
@@ -9,13 +9,18 @@ require.config({　　　　
 	paths: {　　　　　　
 		"jquery": "jquery",
 		"fullPage": "jquery.fullPage.min",
-		"transit": "jquery.transit.min"
+		"transit": "jquery.transit.min",
+		"slide": "responsiveslides.min",
+		"map": "http://api.map.baidu.com/api?v=1.5&ak=KfgGSNmbrkALKFcj3kZh3yYD",
 	},
 	shim: {
 		"fullPage": {
 			deps: ["jquery"]
 		},
 		"transit": {
+			deps: ["jquery"]
+		},
+		"slide": {
 			deps: ["jquery"]
 		},
 	}
@@ -25,13 +30,49 @@ require.config({　　　　
  *Design by Zhongfei.Wei
  *Scripts by Lucas Ren
  */
-require(['jquery', 'fullPage', 'transit'], function($, fullPage, transit) {
+require(['jquery', 'fullPage', 'transit', "slide", "map"], function() {
 	var lily = {
 		distance: 48,
-		whiteCar: [1, 5, 6],
-		blackCar: [2, 3, 4],
+		whiteCar: [1, 3, 5, 6],
+		blackCar: [2, 4],
 		car: $(".icon-48-01"),
 		fullPage: $('#fullpage'),
+		section: $(".section"),
+		header: $("header"),
+		eCar: $(".car"),
+		view1: $(".view-1"),
+		slider: $(".slider"),
+		slideIndex: 0,
+		//car animation & header animation
+		carAnimate: function(index) {
+
+			if (index == 1) {
+				lily.header.transition({
+					height: 100,
+				}, 600, 'easeInOutCubic');
+				lily.header.find(".logo").transition({
+					height: 38,
+					width: 300,
+					margin: "31px auto"
+				}, 600, 'easeInOutCubic');
+			} else {
+				lily.header.transition({
+					height: 60,
+				}, 600, 'easeInOutCubic');
+				lily.header.find(".logo").transition({
+					height: 26,
+					width: 210,
+					margin: "17px auto"
+				}, 600, 'easeInOutCubic');
+			}
+			if (index == 2) {
+				lily.eCar.fadeIn('slow');
+			} else if (index == 3) {
+				lily.eCar.fadeIn('500');
+			} else {
+				lily.eCar.fadeOut('200');
+			}
+		},
 		contains: function(ele) {
 			for (var i = 0; i < lily.blackCar.length; i++) {
 				if (lily.blackCar[i] === ele) {
@@ -48,10 +89,12 @@ require(['jquery', 'fullPage', 'transit'], function($, fullPage, transit) {
 				ele.parent().removeClass("nav-black");
 			ele.transition({
 				y: lily.distance * (index - 1)
-			});
+			}, 600, 'ease');
+			lily.carAnimate(index);
 		},
 		//init fullpage transition
 		init: function() {
+			lily.eCar.hide();
 			lily.fullPage.fullpage({
 				css3: true,
 				easing: 'easeInQuart',
@@ -67,11 +110,29 @@ require(['jquery', 'fullPage', 'transit'], function($, fullPage, transit) {
 			});
 			lily.car.next().find("li").on("click", function() {
 				var index = lily.car.next().find("li").index(this);
-				$.fn.fullpage.moveTo(index+1);
+				$.fn.fullpage.moveTo(index + 1);
+			});
+		},
+		map: function() {
+			var map = new BMap.Map("map");
+			var point = new BMap.Point(116.517089, 39.900591);
+			map.centerAndZoom(point, 16);
+			var marker = new BMap.Marker(point); // 创建标注
+			map.addOverlay(marker); // 将标注添加到地图中
+			marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+		},
+		bgTransition: function() {
+			lily.slider.find("ul").responsiveSlides({
+				timeout: 10000, 
+				speed: 1000
 			});
 		}
+
 	}
+
 	$(document).ready(function() {
 		lily.init();
+		lily.map();
+		lily.bgTransition();
 	});
 });
