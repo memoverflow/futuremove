a832cf99aeccc4ad09e4a26de49bfdba8b639eb9
xuren87 <nichokiki@hotmail.com>
2015-01-30 16:49:41 +0800

    consumer could scroll the page through click element

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@
  */
 // 配置加载项
 require.config({　　　　
-    paths: {　　　　　　
-        "jquery": "jquery",
-        "fullPage": "jquery.fullPage.min",
-        "transit": "jquery.transit.min"
-    },
-    shim: {
-        "fullPage": {
-            deps: ["jquery"]
-        },
-        "transit": {
-            deps: ["jquery"]
-        },
-    }
+	paths: {　　　　　　
+		"jquery": "jquery",
+		"fullPage": "jquery.fullPage.min",
+		"transit": "jquery.transit.min"
+	},
+	shim: {
+		"fullPage": {
+			deps: ["jquery"]
+		},
+		"transit": {
+			deps: ["jquery"]
+		},
+	}
 });
 /**
  *主模块，FutureMove主页面初始化
@@ -26,47 +26,52 @@ require.config({　　　　
  *Scripts by Lucas Ren
  */
 require(['jquery', 'fullPage', 'transit'], function($, fullPage, transit) {
-    var lily = {
-        distance: 48,
-        whiteCar: [1, 5, 6],
-        blackCar: [2, 3, 4],
-        car: $(".icon-48-01"),
-        contains: function(ele) {
-            for (var i = 0; i < lily.blackCar.length; i++) {
-                if (lily.blackCar[i] === ele) {
-                    return true;
-                }
-            }
-            return false;
-        },
-        //navigation movement
-        move: function(ele, index) {
-            if (lily.contains(index))
-                ele.parent().addClass("nav-black");
-            else
-            	ele.parent().removeClass("nav-black");
-            ele.transition({
-                y: lily.distance * (index - 1)
-            });
-        },
-        //init fullpage transition
-        init: function() {
-            $('#fullpage').fullpage({
-                css3: true,
-                easing: 'easeInQuart',
-                easingcss3: 'ease',
-                touchSensitivity: 15,
-                normalScrollElementTouchThreshold: 5,
-                resize: true,
-                verticalCentered: true,
-                keyboardScrolling: true,
-                onLeave: function(index, nextIndex, direction) {
-                    lily.move(lily.car, nextIndex);
-                }
-            });
-        }
-    }
-    $(document).ready(function() {
-        lily.init();
-    });
+	var lily = {
+		distance: 48,
+		whiteCar: [1, 5, 6],
+		blackCar: [2, 3, 4],
+		car: $(".icon-48-01"),
+		fullPage: $('#fullpage'),
+		contains: function(ele) {
+			for (var i = 0; i < lily.blackCar.length; i++) {
+				if (lily.blackCar[i] === ele) {
+					return true;
+				}
+			}
+			return false;
+		},
+		//navigation movement
+		move: function(ele, index) {
+			if (lily.contains(index))
+				ele.parent().addClass("nav-black");
+			else
+				ele.parent().removeClass("nav-black");
+			ele.transition({
+				y: lily.distance * (index - 1)
+			});
+		},
+		//init fullpage transition
+		init: function() {
+			lily.fullPage.fullpage({
+				css3: true,
+				easing: 'easeInQuart',
+				easingcss3: 'ease',
+				touchSensitivity: 15,
+				normalScrollElementTouchThreshold: 5,
+				resize: true,
+				verticalCentered: true,
+				keyboardScrolling: true,
+				onLeave: function(index, nextIndex, direction) {
+					lily.move(lily.car, nextIndex);
+				}
+			});
+			lily.car.next().find("li").on("click", function() {
+				var index = lily.car.next().find("li").index(this);
+				$.fn.fullpage.moveTo(index+1);
+			});
+		}
+	}
+	$(document).ready(function() {
+		lily.init();
+	});
 });
