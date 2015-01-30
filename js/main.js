a27c1803151b7941afc0dea6b3e9df7419ab6e46
xuren87 <nichokiki@hotmail.com>
2015-01-30 16:40:55 +0800

    navigation completed

--- js/main.js
+++ js/main.js
@@ -6,37 +6,67 @@
  */
 // 配置加载项
 require.config({　　　　
-	paths: {　　　　　　
-		"jquery": "jquery",
-		"fullPage": "jquery.fullPage.min"
-	},
-	shim: {
-		"fullPage": {
-			deps: ["jquery"]
-		}
-	}
+    paths: {　　　　　　
+        "jquery": "jquery",
+        "fullPage": "jquery.fullPage.min",
+        "transit": "jquery.transit.min"
+    },
+    shim: {
+        "fullPage": {
+            deps: ["jquery"]
+        },
+        "transit": {
+            deps: ["jquery"]
+        },
+    }
 });
 /**
  *主模块，FutureMove主页面初始化
  *Design by Zhongfei.Wei
  *Scripts by Lucas Ren
  */
-require(['jquery', 'fullPage'], function($, fullPage) {
-	var lily = {
-		//init fullpage transition
-		init:function(){
-			$('#fullpage').fullpage({
-				css3: true,
-       			easing: 'easeInQuart',
-       			easingcss3: 'ease',
-       			touchSensitivity: 15,
-       			normalScrollElementTouchThreshold: 5,
-       			resize : true,
-       			verticalCentered: true,
-       		});
-		}
-	}
-	$(document).ready(function() {
-		lily.init();
-	});
+require(['jquery', 'fullPage', 'transit'], function($, fullPage, transit) {
+    var lily = {
+        distance: 48,
+        whiteCar: [1, 5, 6],
+        blackCar: [2, 3, 4],
+        car: $(".icon-48-01"),
+        contains: function(ele) {
+            for (var i = 0; i < lily.blackCar.length; i++) {
+                if (lily.blackCar[i] === ele) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        //navigation movement
+        move: function(ele, index) {
+            if (lily.contains(index))
+                ele.parent().addClass("nav-black");
+            else
+            	ele.parent().removeClass("nav-black");
+            ele.transition({
+                y: lily.distance * (index - 1)
+            });
+        },
+        //init fullpage transition
+        init: function() {
+            $('#fullpage').fullpage({
+                css3: true,
+                easing: 'easeInQuart',
+                easingcss3: 'ease',
+                touchSensitivity: 15,
+                normalScrollElementTouchThreshold: 5,
+                resize: true,
+                verticalCentered: true,
+                keyboardScrolling: true,
+                onLeave: function(index, nextIndex, direction) {
+                    lily.move(lily.car, nextIndex);
+                }
+            });
+        }
+    }
+    $(document).ready(function() {
+        lily.init();
+    });
 });
